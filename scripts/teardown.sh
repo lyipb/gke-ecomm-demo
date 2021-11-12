aec132ea101980dd2c2b4e7befab6fd42e44783f
lyipb <lyipb@mac.air>
2021-11-12 19:48:34 +0800

    update scripts: generate.sh, teardown.sh

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -4,29 +4,52 @@
 ##
 
 # Delete mongod stateful set + mongodb service + secrets + host vm configuer daemonset
-kubectl delete statefulsets mongod
-kubectl delete services mongodb-service
-kubectl delete secret shared-bootstrap-data
-kubectl delete daemonset hostvm-configurer
-sleep 3
+# kubectl delete statefulsets mongod
+# kubectl delete services mongodb-service
+# kubectl delete secret shared-bootstrap-data
+# kubectl delete daemonset hostvm-configurer
+# sleep 3
 
-# Delete persistent volume claims
-kubectl delete persistentvolumeclaims -l role=mongo
-sleep 3
-
-# Delete persistent volumes
-for i in 1 2 3
+# Delete persistent volume claims for pc
+for i in  0 1 2 
 do
-    kubectl delete persistentvolumes data-volume-$i
+    kubectl delete pvc mongodb-persistent-storage-claim-pc-mongo-$i
 done
 sleep 20
 
-# Delete GCE disks
-for i in 1 2 3
+# Delete persistent volume claims for pi
+for i in  0 1 2 
 do
-    gcloud -q compute disks delete pd-ssd-disk-$i
+    kubectl delete pvc mongodb-persistent-storage-claim-pi-mongo-$i
+done
+sleep 20
+
+# Delete persistent volumes for pc
+for i in 0 1 2 
+do
+    kubectl delete persistentvolumes data-volume-pc-$i
+done
+sleep 20
+
+# Delete persistent volumes for pi
+for i in 0 1 2 
+do
+    kubectl delete persistentvolumes data-volume-pi-$i
+done
+sleep 20
+
+# Delete GCE disks for pc
+for i in 0 1 2 
+do
+    gcloud -q compute disks delete pd-ssd-disk-pc-$i --zone=us-central1-c
+done
+
+# Delete GCE disks for pi
+for i in 0 1 2 
+do
+    gcloud -q compute disks delete pd-ssd-disk-pi-$i --zone=us-central1-c
 done
 
 # Delete whole Kubernetes cluster (including its VM instances)
-gcloud -q container clusters delete "gke-mongodb-demo-cluster"
+gcloud -q container clusters delete "gke-ecommerce-demo-cluster"
 
